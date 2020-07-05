476ff007fd8a35c470032e356d4b9b47b57d0471
Vittorio Fones <vittorio.fones@gmail.com>
2020-07-05 13:30:28 +0200

    Changing printing frequency and saving freq.

--- sr.py
+++ sr.py
@@ -28,8 +28,9 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, enco
 
   for epoch in range(max_epoch):
     print('Starting epoch: ', epoch,
-          ', current learning rate: ', scheduler.get_last_lr())
-
+          ', current learning rate: ', scheduler.get_last_lr(),
+          ', learning rates: ', scheduler.get_lr())
+  
     for i, (_, img, verb, labels) in enumerate(train_loader):
       total_steps += 1
 
@@ -37,22 +38,15 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, enco
       verb = torch.autograd.Variable(verb.cuda())
       labels = torch.autograd.Variable(labels.cuda())
         
-      #if verbose flag is set and iterated 40 images then print
+      #if verbose flag is set and iterated 256 images then print
       if total_steps % 256 == 0 and verbose:
         print_flag = True
 
-      if print_flag is True:
-        print('Predicting roles in frame')
         
       role_predict = model(img, verb)
 
-      if print_flag is True:
-        print('Calculating loss')
-        
       loss = model.module.calculate_loss(verb, role_predict, labels)
 
-      if print_flag is True:
-        print('Backpropragating through time')
         
       loss.backward()
 
@@ -70,16 +64,16 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, enco
       if print_flag is True:
         top1_a = top1.get_average_results_nouns()
         top5_a = top5.get_average_results_nouns()
-        print("Total_steps: {}, elements n°: {}, {} , {}, loss = {:.2f}, avg loss = {:.2f}"
-          .format(total_steps-1, i,
+        print("Total_steps: {}, {} , {}, loss = {:.2f}, avg loss = {:.2f}"
+          .format(total_steps,
           utils.format_dict(top1_a, "{:.2f}", "1-"),
           utils.format_dict(top5_a,"{:.2f}","5-"),
-          loss.item(), train_loss/((total_steps-1)%10000)
+          loss.item(), train_loss/len(train_loader)
           )
         )
 
 
-      if total_steps % 10000 == 0:
+      if total_steps % 256 == 0:
         top1, top5, val_loss = eval(model, dev_loader, encoder)
         model.train()
 
@@ -90,9 +84,10 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, enco
                     top5_avg["value"] + top5_avg["value-all"] + top5_avg["value*"] + top5_avg["value-all*"]
         avg_score /= 8
 
-        print ('Dev {} average :{:.2f} {} {}'.format(total_steps-1, avg_score*100,
-                                                      utils.format_dict(top1_avg,'{:.2f}', '1-'),
-                                                      utils.format_dict(top5_avg, '{:.2f}', '5-')))
+        print ('Dev average :{:.2f} {} {}'.format(avg_score*100,
+                                        utils.format_dict(top1_avg,'{:.2f}', '1-'),
+                                        utils.format_dict(top5_avg, '{:.2f}', '5-')))
+        
         dev_score_list.append(avg_score)
         max_score = max(dev_score_list)
 
@@ -107,15 +102,14 @@ def train(model, train_loader, dev_loader, optimizer, scheduler, max_epoch, enco
 
           print ('New best model saved! {0}'.format(max_score))
 
-        print('current train loss', train_loss)
-        train_loss = 0
+        print('current train loss', train_loss/len(train_loader))
+        #train_loss = 0
         top1 = imsitu_scorer.imsitu_scorer(encoder, 1, 3)
         top5 = imsitu_scorer.imsitu_scorer(encoder, 5, 3)
 
       if print_flag is True:
         print_flag = False
       
-    print('batches size: ', i)
     #del role_predict, loss, img, verb, labels
     
   scheduler.step()
@@ -143,62 +137,49 @@ def eval(model, dev_loader, encoder, write_to_file = False):
         top1.add_point_noun(verb, role_predict, labels)
         top5.add_point_noun(verb, role_predict, labels)
 
-      del role_predict, img, verb, labels
+      #del role_predict, img, verb, labels
 
   return top1, top5, 0
 
 if __name__ == "__main__":
   import argparse
-  parser = argparse.ArgumentParser(description="imsitu VSRL. Training, evaluation and prediction.")
+  parser = argparse.ArgumentParser(description="Situation recognition GGNN. Training, evaluation and prediction.")
   parser.add_argument('--resume_training', action='store_true', help='Resume training from the model [resume_model]')
   parser.add_argument('--resume_model', type=str, default='', help='The model we resume')
   parser.add_argument('--evaluate', action='store_true', help='Only use the testing mode')
   parser.add_argument('--test', action='store_true', help='Only use the testing mode')
-  parser.add_argument('--dataset_folder', type=str, default='./imSitu', help='Location of annotations')
-  parser.add_argument('--imgset_dir', type=str, default='./resized_256', help='Location of original images')
-  parser.add_argument('--train_file', default="train.json", type=str, help='trainfile name')
-  parser.add_argument('--dev_file', default="dev.json", type=str, help='dev file name')
-  parser.add_argument('--test_file', default="test.json", type=str, help='test file name')
   parser.add_argument('--model_saving_name', type=str, help='saving name of the outpul model')
   parser.add_argument('--verbose', action='store_true', help='set verbose mode')
 
   parser.add_argument('--epochs', type=int, default=500)
   parser.add_argument('--seed', type=int, default=1111, help='random seed')
   parser.add_argument('--clip_norm', type=float, default=0.25)
-  parser.add_argument('--num_workers', type=int, default=3)
 
   args = parser.parse_args()
 
   n_epoch = args.epochs
-  batch_size = 64 * torch.cuda.device_count()
   clip_norm = args.clip_norm
-  n_worker = args.num_workers
-
-  dataset_folder = args.dataset_folder
-  imgset_folder = args.imgset_dir
-
-  train_set = json.load(open(dataset_folder + '/' + args.train_file))
 
   encoder = imsitu_encoder.imsitu_encoder(train_set)
 
   model = model.build_ggnn_baseline(encoder.get_num_roles(), encoder.get_num_verbs(), encoder.get_num_labels(), encoder)
   
-  train_set = imsitu_loader.imsitu_loader(imgset_folder, train_set, encoder,'train', encoder.train_transform)
-  train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=n_worker)
+  train_set = json.load(open('imSitu/train.json'))
+  train_set = imsitu_loader.imsitu_loader('resized_256', train_set, encoder,'train', encoder.train_transform)
+  train_loader = torch.utils.data.DataLoader(train_set, batch_size=256, shuffle=True, num_workers=16)
 
-  dev_set = json.load(open(dataset_folder + '/' + args.dev_file))
-  dev_set = imsitu_loader.imsitu_loader(imgset_folder, dev_set, encoder, 'val', encoder.dev_transform)
-  dev_loader = torch.utils.data.DataLoader(dev_set, batch_size=batch_size, shuffle=True, num_workers=n_worker)
+  dev_set = json.load(open('imSitu/dev.json'))
+  dev_set = imsitu_loader.imsitu_loader('resized_256', dev_set, encoder, 'val', encoder.dev_transform)
+  dev_loader = torch.utils.data.DataLoader(dev_set, batch_size=256, shuffle=True, num_workers=16)
 
-  test_set = json.load(open(dataset_folder + '/' + args.test_file))
-  test_set = imsitu_loader.imsitu_loader(imgset_folder, test_set, encoder, 'test', encoder.dev_transform)
-  test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=n_worker)
+  test_set = json.load(open('imSitu/test.json'))
+  test_set = imsitu_loader.imsitu_loader('resized_256', test_set, encoder, 'test', encoder.dev_transform)
+  test_loader = torch.utils.data.DataLoader(test_set, batch_size=256, shuffle=True, num_workers=16)
 
   if not os.path.exists('trained_models'):
     os.mkdir('trained_models')
   checkpoint = None
 
-  torch.manual_seed(args.seed)
     
   model.cuda()
   torch.cuda.manual_seed(args.seed)
