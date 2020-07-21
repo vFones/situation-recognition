Fixed backpropagation and criterions

model.py
@@ -82,13 +82,12 @@ class FCGGNN(nn.Module):
 
     self.verb_classifier = nn.Sequential(
       nn.Dropout(0.5),
-      nn.Linear(D_hidden_state, encoder.get_num_verbs()),
-      nn.Linear(encoder.get_num_verbs(), 1)
+      nn.Linear(D_hidden_state, self.encoder.get_num_verbs())
     )
 
     self.nouns_classifier = nn.Sequential(
       nn.Dropout(0.5),
-      nn.Linear(D_hidden_state, encoder.get_num_labels())
+      nn.Linear(D_hidden_state, self.encoder.get_num_labels())
     )
 
 
@@ -128,9 +127,6 @@ class FCGGNN(nn.Module):
   def __predict_verb(self, img_features, batch_size):
     img_features = torch.nn.functional.relu(img_features)
     out = self.verb_classifier(img_features)
-    out = out.contiguous().view(batch_size, -1)
-    
-    # return predicted verb based on images in batch
     return out
 
 
@@ -140,7 +136,9 @@ class FCGGNN(nn.Module):
     batch_size = img_features.size(0)
 
     pred_verb = self.__predict_verb(img_features, batch_size)
-    pred_nouns = self.__predict_nouns(img_features,  torch.argmax(pred_verb, dim=1), batch_size)
+    _, verb = torch.max(pred_verb, 1)
+
+    pred_nouns = self.__predict_nouns(img_features, verb, batch_size)
     gt_pred_nouns = self.__predict_nouns(img_features, gt_verb, batch_size)
     
     return pred_verb, pred_nouns, gt_pred_nouns
@@ -153,7 +151,7 @@ class FCGGNN(nn.Module):
     verb_pred_criterion = nn.CrossEntropyLoss()
     nouns_pred_criterion = nn.CrossEntropyLoss(ignore_index=self.encoder.get_num_labels())
     gt_pred_criterion = nn.CrossEntropyLoss(ignore_index=self.encoder.get_num_labels())
-    
+
     verb_loss = verb_pred_criterion(pred_verb, gt_verb)
 
     gt_label_turned = gt_nouns.transpose(1,2).contiguous().view(batch_size* self.encoder.max_role_count*3, -1)

utils/imsitu_scorer.py
@@ -78,7 +78,8 @@ class imsitu_scorer():
       new_card = {"verb":0.0, "value":0.0, "value-all":0.0, "gt-value":0.0, "gt-value-all":0.0}
       score_card = new_card
 
-      if torch.equal(pred_verb, verbs[i]):
+      verb_found = False
+      if torch.equal(verbs[i], pred_verb.long()):
         verb_found = True
       
       if verb_found:
@@ -93,9 +94,9 @@ class imsitu_scorer():
         label_id = torch.topk(pred_noun[k], k=self.topk)[1]
         gt_label_id = torch.topk(gt_pred_noun[k], k=self.topk)[1]
         for r in range(0, 3):
-          if label_id == label[r][k]:
+          if torch.equal(label_id, label[r][k]):
             found += 1
-          if gt_label_id == label[r][k]:
+          if torch.equal(gt_label_id, label[r][k]):
             gt_found += 1
         
         if found < (3 * self.topk):