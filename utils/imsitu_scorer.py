025fdfbd7f2a4368d1cf243d0456320580d686db
Vittorio Fones <vittorio.fones@gmai.com>
2020-07-30 01:54:31 +0200

    Fixed scoring insert

--- utils/imsitu_scorer.py
+++ utils/imsitu_scorer.py
@@ -22,13 +22,13 @@ class imsitu_scorer():
       pred_verb = pred_verbs[i]
       verb = verbs[i]
 
-      pred_role_noun = pred_roles_nouns[i]
-      gt_pred_role_noun = gt_pred_roles_nouns[i]
+      pred_role_nouns = pred_roles_nouns[i]
+      gt_pred_role_nouns = gt_pred_roles_nouns[i]
 
       role_noun = roles_nouns[i]
 
       _, pred_verb_idx = torch.topk(pred_verb, self.topk)
-      _, pred_role_noun_idx = torch.topk(pred_roles_nouns, self.topk)      
+      _, pred_role_noun_idx = torch.topk(pred_role_nouns, self.topk)      
       
       gt_roles_count = self.encoder.get_role_count(verb)
 
@@ -46,7 +46,7 @@ class imsitu_scorer():
         for r in range(0, gt_roles_count):
           #for all nouns in three annotations
           for n in range(0, 3):
-            if pred_role_noun_idx[0][r][k] == role_noun[n][r]:
+            if pred_role_noun_idx[r][k] == role_noun[n][r]:
               found += 1
        
         if found >= gt_roles_count:
@@ -56,11 +56,11 @@ class imsitu_scorer():
         if all_found: new_card["value-all"] += 1
       
       if self.topk == 1:
-        _, gt_pred_role_noun_idx = torch.topk(gt_pred_roles_nouns, 1)
+        _, gt_pred_role_noun_idx = torch.topk(gt_pred_role_nouns, 1)
         for r in range(0, gt_roles_count):
           #for all nouns in three annotations
           for n in range(0, 3):
-            if gt_pred_role_noun_idx[0][r][0] == role_noun[n][r]:
+            if gt_pred_role_noun_idx[r][0] == role_noun[n][r]:
               gt_found += 1
         
         if gt_found >= gt_roles_count:
