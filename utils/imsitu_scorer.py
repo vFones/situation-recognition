b6d824435c06ebca71223705f599d24afb14218d
Vittorio Fones <vittorio.fones@gmail.com>
2020-08-05 10:43:41 +0200

    Updated scorer

--- utils/imsitu_scorer.py
+++ utils/imsitu_scorer.py
@@ -31,13 +31,11 @@ class imsitu_scorer():
       _, pred_role_noun_idx = torch.topk(pred_role_nouns, self.topk)      
       
       gt_roles_count = self.encoder.get_role_count(verb)
-
+      
       for k in range(0, self.topk):
         found = 0
         gt_found = 0
 
-        all_found = False
-        gt_all_found = False
 
         if pred_verb_idx[k] == verb:
           new_card["verb"] += 1
@@ -46,14 +44,16 @@ class imsitu_scorer():
         for r in range(0, gt_roles_count):
           #for all nouns in three annotations
           for n in range(0, 3):
+            #print("pred_role_noun_idx double for: ", pred_role_noun_idx[r][k], role_noun[n][r])
+            #print(self.encoder.label_list[pred_role_noun_idx[r][k]], self.encoder.label_list[role_noun[n][r]])
             if pred_role_noun_idx[r][k] == role_noun[n][r]:
               found += 1
-       
-        if found >= gt_roles_count:
-          all_found = True
 
-        if found > 0: new_card["value"] += 1
-        if all_found: new_card["value-all"] += 1
+        if found >= 3:
+          new_card["value-all"] += 1
+
+        if found > 0:
+          new_card["value"] += 1
       
       if self.topk == 1:
         _, gt_pred_role_noun_idx = torch.topk(gt_pred_role_nouns, 1)
@@ -63,11 +63,11 @@ class imsitu_scorer():
             if gt_pred_role_noun_idx[r][0] == role_noun[n][r]:
               gt_found += 1
         
-        if gt_found >= gt_roles_count:
-            gt_all_found = True
+        if gt_found >= 3:
+          new_card["gt-value-all"] += 1
 
-        if gt_found > 0: new_card["gt-value"] += 1
-        if gt_all_found: new_card["gt-value-all"] += 1
+        if gt_found > 0:
+          new_card["gt-value"] += 1 
 
       new_card["value"] /= self.topk
       new_card["value-all"] /= self.topk
