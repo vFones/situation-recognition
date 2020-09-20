ec384b5c459c4cc0eb2759e59cbce5fdde34f995
Vittorio Fones <vittorio.fones@gmai.com>
2020-09-20 19:23:38 +0200

    Refactoring prints and stuff

--- sr.py
+++ sr.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from PIL import Image
 
-
 from model import FCGGNN
 from utils import imsitu_encoder, imsitu_loader, imsitu_scorer, utils
 
@@ -19,6 +18,7 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, encoder, model_
   val_avg_scores = []
   val_verb_losses = []
   val_nouns_losses = []
+
   epoch = 0
   
   if checkpoint is not None:
@@ -71,14 +71,14 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, encoder, model_
           nouns_loss =  model.nouns_loss(pred_nouns, nouns)
           gt_nouns_loss =  model.nouns_loss(pred_gt_nouns, nouns)
         
-        loss = verb_loss+nouns_loss
+      loss = verb_loss+nouns_loss
       
       scaler.scale(loss).backward()
       scaler.unscale_(optimizer)
       torch.nn.utils.clip_grad_norm_(model.parameters(), 1)    
       scaler.step(optimizer)
       scaler.update()
-  
+
       top1.add_point_both(pred_verb, verb, pred_nouns, nouns, pred_gt_nouns)
       top5.add_point_both(pred_verb, verb, pred_nouns, nouns, pred_gt_nouns)
 
@@ -117,23 +117,14 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, encoder, model_
                 utils.format_dict(gt, '{:.2f}', ''), avg_score, '-'*50))
 
     # evaluating 
-    top1, top5, val_losses = eval(model, dev_loader, encoder)
+    top1, top5, val_losses, val_avg_score = eval(model, dev_loader, encoder, logging=True)
     model.train()
 
     #val mean scores
-    top1_a = top1.get_average_results_both()
-    top5_a = top5.get_average_results_both()
-    avg_score = top1_a['verb'] + top1_a['value'] + top1_a['value-all'] + \
-                top5_a['verb'] + top5_a['value'] + top5_a['value-all'] + \
-                top1_a['gt-value'] + top1_a['gt-value-all']
-    avg_score /= 8
-    val_avg_score = avg_score * 100
     val_avg_scores.append(val_avg_score)
-
-    #plots
     val_verb_losses.append(val_losses['verb_loss'])
     val_nouns_losses.append(val_losses['nouns_loss'])
-    
+
 
     plt.plot(verb_losses, label='verb losses')
     plt.plot(nouns_losses, label='nouns losses')
@@ -146,17 +137,6 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, encoder, model_
     plt.legend()
     plt.savefig(pjoin(folder, model_saving_name+'.png'))
     plt.clf()
-
-    print('validation losses = [v: {:.2f}, n: {:.2f}, gt: {:.2f}]'
-      .format(val_losses['verb_loss'], val_losses['nouns_loss'], val_losses['gt_loss']))
-      
-    gt = {key:top1_a[key] for key in ['gt-value', 'gt-value-all']}
-    one_val = {key:top1_a[key] for key in ['verb', 'value', 'value-all']}
-    print('{}\n{}\n{}, mean = {:.2f}\n{}'
-      .format(utils.format_dict(one_val, '{:.2f}', '1-'),
-                utils.format_dict(top5_a, '{:.2f}', '5-'),
-                utils.format_dict(gt, '{:.2f}', ''), val_avg_score, '-'*50))
-
 
     checkpoint = { 
       'epoch': e+1,
@@ -176,14 +156,12 @@ def train(model, train_loader, dev_loader, optimizer, max_epoch, encoder, model_
       checkpoint.update({'model_state_dict': model.module.state_dict()})
     if scheduler is not None:
       checkpoint['scheduler_state_dict'] = scheduler.state_dict()
-    
-    torch.save(checkpoint, pjoin(folder, model_saving_name))
-      
-    if scheduler is not None:
       scheduler.step()
 
+    torch.save(checkpoint, pjoin(folder, model_saving_name))
 
-def eval(model, loader, encoder):
+
+def eval(model, loader, encoder, logging=False):
   model.eval()
 
   verbloss = 0
@@ -221,9 +199,30 @@ def eval(model, loader, encoder):
   verbloss /= len(loader)
   nounsloss /= len(loader)
   gtloss /= len(loader)
-  val_loss = {'verb_loss':verbloss, 'nouns_loss': nounsloss, 'gt_loss': gtloss}
+  val_losses = {'verb_loss':verbloss, 'nouns_loss': nounsloss, 'gt_loss': gtloss}
+  
+  avg_score = 0
+  if logging is True:
+    top1_a = top1.get_average_results_both()
+    top5_a = top5.get_average_results_both()
 
-  return top1, top5, val_loss
+    avg_score = top1_a['verb'] + top1_a['value'] + top1_a['value-all'] + \
+                top5_a['verb'] + top5_a['value'] + top5_a['value-all'] + \
+                top1_a['gt-value'] + top1_a['gt-value-all']
+    avg_score /= 8
+    avg_score = avg_score * 100
+
+    print('val losses = [v: {:.2f}, n: {:.2f}, gt: {:.2f}]'
+      .format(val_losses['verb_loss'], val_losses['nouns_loss'], val_losses['gt_loss']))
+
+    gt = {key:top1_a[key] for key in ['gt-value', 'gt-value-all']}
+    one_val = {key:top1_a[key] for key in ['verb', 'value', 'value-all']}
+    print('{}\n{}\n{}, mean = {:.2f}\n'
+      .format(utils.format_dict(one_val, '{:.2f}', '1-'),
+              utils.format_dict(top5_a, '{:.2f}', '5-'),
+              utils.format_dict(gt, '{:.2f}', ''), avg_score))
+
+  return top1, top5, val_losses, avg_score
 
 def results(model, image, encoder):
   model.eval()
@@ -258,26 +257,25 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Situation recognition GGNN. Training, evaluation and prediction.')
   parser.add_argument('--resume_model', type=str, default='', help='The model we resume')
   
-  parser.add_argument('--benchmark', action='store_true', help='Only use the benchmark mode')
-  parser.add_argument('--evaluate', action='store_true', help='Only use the testing mode')
-  parser.add_argument('--results', action='store_true', help='Only use the results mode')
-  parser.add_argument('--test', action='store_true', help='Only use the testing mode')
-  parser.add_argument('--img', type=str, default='', help='Load a picture')
-  parser.add_argument('--model_saving_name', type=str, default='sr', help='saving name of the outpul model')
+  parser.add_argument('--evaluate_dev', action='store_true', help='Only use the testing mode')
+  parser.add_argument('--evaluate_test', action='store_true', help='Only use the testing mode')
 
+  parser.add_argument('--results', action='store_true', help='Only use the results mode')
+  parser.add_argument('--img', type=str, default='', help='Load a picture')
+
+  parser.add_argument('--model_saving_name', type=str, default='sr', help='saving name of the outpul model')
   parser.add_argument('--saving_folder', type=str, default='checkpoints', help='Location of annotations')
-  parser.add_argument('--dataset_folder', type=str, default='imSitu', help='Location of annotations')
   parser.add_argument('--imgset_dir', type=str, default='resized_256', help='Location of original images')
+  parser.add_argument('--dataset_folder', type=str, default='imSitu', help='Location of annotations')
 
   parser.add_argument('--train_file', type=str, default='train.json', help='Train json file')
   parser.add_argument('--dev_file', type=str, default='dev.json', help='Dev json file')
   parser.add_argument('--test_file', type=str, default='test.json', help='test json file')
   
-  
   parser.add_argument('--batch_size', type=int, default=384)
   parser.add_argument('--num_workers', type=int, default=10)
 
-  parser.add_argument('--epochs', type=int, default=100)
+  parser.add_argument('--epochs', type=int, default=1000)
   parser.add_argument('--lr', type=float, default=0.25118864315095822) 
 
   args = parser.parse_args()
@@ -323,7 +321,13 @@ if __name__ == '__main__':
     model = torch.nn.DataParallel(model)
     model.cuda()
 
+  optimizer = torch.optim.Adamax(filter(lambda p: p.requires_grad, model.parameters()))
+  
+  scheduler = None
   torch.backends.cudnn.benchmark = True
+
+
+  e=-1
   if len(args.resume_model) > 1:
     print('Resume training from: {}'.format(args.resume_model))
     if torch.cuda.is_available():
@@ -333,69 +337,34 @@ if __name__ == '__main__':
     path_to_model = pjoin(args.saving_folder, args.resume_model)
     checkpoint = torch.load(path_to_model,  map_location=device)
     utils.load_net(path_to_model, [model])
+
+    for parameter in model.convnet_verbs.parameters():
+      parameter.requires_grad = False
+    model.convnet_verbs.model.fc.requires_grad = True
+
+    for parameter in model.convnet_nouns.parameters():
+      parameter.requires_grad = False
+    model.convnet_nouns.model.fc.requires_grad = True
+
     args.model_saving_name = args.resume_model
-  else:
-    print('Training from the scratch.')
-    utils.set_trainable(model, True)
+    e = checkpoint['epoch']
   
-  
-  optimizer = torch.optim.SGD(model.parameters(),
-    lr=args.lr, momentum=0.9, nesterov=True, weight_decay=1e-4)
-  
-  scheduler = None
-  scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=args.lr*0.01, max_lr=args.lr,
-    step_size_up=3*len(train_loader), cycle_momentum=False, mode='exp_range')
- 
-  if args.evaluate:
+  #scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=args.lr*0.001, max_lr=args.lr,
+  #  step_size_up=2*len(train_loader), mode='exp_range')
+  #scheduler.last_epoch = e
+
+  if args.evaluate_dev:
     print ('=> evaluating model with dev-set...')
-    top1, top5, val_losses = eval(model, dev_loader, encoder)
-
-    top1_a = top1.get_average_results_both()
-    top5_a = top5.get_average_results_both()
-
-    avg_score = top1_a['verb'] + top1_a['value'] + top1_a['value-all'] + \
-                top5_a['verb'] + top5_a['value'] + top5_a['value-all'] + \
-                top1_a['gt-value'] + top1_a['gt-value-all']
-    avg_score /= 8
-
-    print('val losses = [v: {:.2f}, n: {:.2f}, gt: {:.2f}]'
-      .format(val_losses['verb_loss'], val_losses['nouns_loss'], val_losses['gt_loss']))
- 
-    gt = {key:top1_a[key] for key in ['gt-value', 'gt-value-all']}
-    one_val = {key:top1_a[key] for key in ['verb', 'value', 'value-all']}
-    print('{}\n{}\n{}, mean = {:.2f}\n'
-      .format(utils.format_dict(one_val, '{:.2f}', '1-'),
-              utils.format_dict(top5_a, '{:.2f}', '5-'),
-              utils.format_dict(gt, '{:.2f}', ''), avg_score*100))
-
-
-  elif args.test:
+    top1, top5, val_losses, avg_score = eval(model, dev_loader, encoder, logging=True)
+  
+  if args.evaluate_test:
     print ('=> evaluating model with test-set...')
-
-    top1, top5, test_losses = eval(model, test_loader, encoder)
-
-    top1_a = top1.get_average_results_both()
-    top5_a = top5.get_average_results_both()
-
-    avg_score = top1_a['verb'] + top1_a['value'] + top1_a['value-all'] + \
-                top5_a['verb'] + top5_a['value'] + top5_a['value-all'] + \
-                top1_a['gt-value'] + top1_a['gt-value-all']
-    avg_score /= 8
-
-    print('test losses = [v: {:.2f}, n: {:.2f}, gt: {:.2f}]'
-      .format(test_losses['verb_loss'], test_losses['nouns_loss'], test_losses['gt_loss']))
-     
-    gt = {key:top1_a[key] for key in ['gt-value', 'gt-value-all']}
-    one_val = {key:top1_a[key] for key in ['verb', 'value', 'value-all']}
-    print('{}\n{}\n{}, mean = {:.2f}\n'
-      .format(utils.format_dict(one_val, '{:.2f}', '1-'),
-              utils.format_dict(top5_a, '{:.2f}', '5-'),
-              utils.format_dict(gt, '{:.2f}', ''), avg_score*100))
+    top1, top5, val_losses, avg_score = eval(model, test_loader, encoder, logging=True)
 
   if args.results:
     verb, labels= results(model, args.img, encoder, train_set)
     print("The verb is :", verb, " the labels are :", labels)
 
   else:
-      print('Model training started!')
-      train(model, train_loader, dev_loader, optimizer, n_epoch, encoder, args.model_saving_name, folder=args.saving_folder, scheduler=scheduler, checkpoint=checkpoint)
+    print('Model training started!')
+    train(model, train_loader, dev_loader, optimizer, n_epoch, encoder, args.model_saving_name, folder=args.saving_folder, scheduler=scheduler, checkpoint=checkpoint)
